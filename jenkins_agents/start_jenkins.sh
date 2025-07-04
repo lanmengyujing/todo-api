88ee588499089157a0a3f830394e92f7b1657661
Simon <simonwantirna@gmail.com>
2025-07-04 11:40:10 +1000

    add Jenkins config

---
+++ jenkins_agents/start_jenkins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+docker rm -f jenkins || true
+
+docker run -d \
+  --name jenkins \
+  -p 8080:8080 -p 50000:50000 \
+  -v "$(pwd)/jenkins_home":/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  jenkins/jenkins:lts
